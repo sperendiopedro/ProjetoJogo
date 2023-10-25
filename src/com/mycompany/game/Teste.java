67793822dc3fc5d346a87c30cf499057b6308c2e
Pedro Sperendio <pedro.sperendio@outlook.com>
2023-10-25 15:05:04 -0300

    Implementação do SWING no projeto

---
+++ src/com/mycompany/game/Teste.java
@@ -0,0 +1,49 @@
+package com.mycompany.game;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+
+public class Teste extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Teste frame = new Teste();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Teste() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(500, 200, 600, 500); //first two number are the location, the last two numbers are the size.
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Test Button");
+		btnNewButton.setBounds(187, 306, 169, 21);
+		contentPane.add(btnNewButton);
+	}
+}
